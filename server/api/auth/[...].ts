8fc572b052fdf1ae3d0adf3bf3bd1f7cc5ae8257
Nick <kuznaik@gmail.com>
2023-04-11 19:44:41 -0500

    dochints

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -14,12 +14,14 @@ export default NuxtAuthHandler({
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
+      // https://github.com/settings/developers
       clientId: envConfig.AUTH_GITHUB_CLIENT_ID,
       clientSecret: envConfig.AUTH_GITHUB_CLIENT_SECRET,
     }),
 
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     Auth0Provider.default({
+      // https://manage.auth0.com/dashboard
       clientId: envConfig.AUTH_AUTH0_CLIENT_ID,
       clientSecret: envConfig.AUTH_AUTH0_CLIENT_SECRET,
       issuer: envConfig.AUTH_AUTH0_ISSUER,
